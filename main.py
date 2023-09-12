6de8b7c1b91f8957181d9b5c58c70d9118c28273
Marach8 <nnannamarach4@gmail.com>
2023-09-12 11:52:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print('Infinity Dice 🎲')
+print()
+
+def rollDice(sides):
+  import random
+  while True:
+    a = random.randint(0, sides)
+    print(f'You rolled {a}')
+    print()
+    b = input('Do you want to roll again?: ').lower()
+    print()
+    if b == 'yes':
+      continue
+    else:
+      print('Bye! See you next time😍')
+      break
+no_of_sides = int(input('How many sides do you want your dice to have?: '))
+print()
+rollDice(no_of_sides)
